b3f3cfaf37ed3d774c3e8925968162350220600f
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-03-01 12:42:42 +0100

    Added today's exercise.

---
+++ js/app.js
@@ -0,0 +1,34 @@
+console.log('Hello Javascript')
+
+//ESERCIZIO 1
+
+let number = 100; //number
+
+console.log(number);
+
+//ESERCIZIO 2
+
+const PI = 3.14; //number
+
+console.log(PI)
+
+//ESERCIZIO 2/B
+
+//Il valore di una variabile "const" non può essere modificato, il browser darà un messaggio d'errore.
+
+//ESERCIZIO 2/C
+
+let radius = 8; //number
+let circle = (radius * 2 * PI); //number
+
+console.log(circle);
+
+//ESERCIZIO 3
+
+let name = 'Carlo'; //string
+name = 'Marco'; //string
+
+console.log(name);
+
+
+
